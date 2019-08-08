33db5e93930d2f618ed70579c8fb21376f59d37e
LukashinAlexey <alexlukus@outlook.com>
2019-08-08 22:16:37 +0300

    using bootstrap 4

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -2,17 +2,6 @@ import { Component} from '@angular/core';
   
 @Component({
     selector: 'home-app',
-    template: `<div class="container">
-    <div class="jumbotron">
-      <h1>Welcome</h1>
-      <h2>Angular & Bootstrap Demo</h2>
-    </div>
-    <div class="panel panel-primary">
-      <div class="panel-heading">Status</div>
-      <div class="panel-body">
-        <h3>{{title}}</h3>
-      </div>
-    </div>
-  </div>`
+    templateUrl: './home.component.html'
 })
 export class HomeComponent { }
