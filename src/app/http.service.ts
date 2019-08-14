9c6911767cfae1703e55e04b6c00e60ddfb1c5aa
LukashinAlexey <alexlukus@outlook.com>
2019-08-14 18:27:23 +0300

    some changes

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -13,19 +13,19 @@ export class HttpService {
         return this.http.get('http://localhost:54058/api/devices');
     }
     deleteDevice(device: Device) {
-        const body = { mac: device.mac};
-        return this.http.delete('http://localhost:54058/api/devices/delete'+ body);
-    }    
+        const body = { mac: device.mac };
+        return this.http.delete('http://localhost:54058/api/devices/delete' + body);
+    }
     getSensors() {
         return this.http.get('http://localhost:54058/api/sensors');
     }
     deleteSensor(sensor: Sensor) {
-        const body = { mac: sensor.mac};
-        return this.http.delete('http://localhost:54058/api/sensors/delete'+ body);
+        const body = { mac: sensor.mac };
+        return this.http.delete('http://localhost:54058/api/sensors/delete' + body);
     }
     postRunMethod(baseHouseSlaveInvoker: BaseHouseSlaveInvoker) {
-        const body = { connectionId: baseHouseSlaveInvoker.connectionId, name: baseHouseSlaveInvoker.name, address: baseHouseSlaveInvoker.address};
+        const body = { connectionId: baseHouseSlaveInvoker.connectionId, name: baseHouseSlaveInvoker.name, address: baseHouseSlaveInvoker.address };
         return this.http.post('http://localhost:54058/api/devices/run', body);
     }
-    
+
 }
