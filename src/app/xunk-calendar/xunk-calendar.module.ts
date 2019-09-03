b91f0a771c812b0f4d899fcfb3ab6615a2675241
Alex Lukashun <alexlukus@outlook.com>
2019-09-03 12:00:42 +0300

    code formatting

--- src/app/xunk-calendar/xunk-calendar.module.ts
+++ src/app/xunk-calendar/xunk-calendar.module.ts
@@ -1,34 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 
-import { CommonModule } from '@angular/common';
+import { CommonModule } from "@angular/common";
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 
-import { XunkCalendarComponent } from './xunk-calendar.component';
+import { XunkCalendarComponent } from "./xunk-calendar.component";
 
 @NgModule({
-    declarations: [
-        XunkCalendarComponent
-    ],
-    imports: [
-        CommonModule,
-        MatIconModule,
-        MatButtonModule,
-     ],
-    exports: [
-        XunkCalendarComponent,
-    ]
+  declarations: [XunkCalendarComponent],
+  imports: [CommonModule, MatIconModule, MatButtonModule],
+  exports: [XunkCalendarComponent]
 })
 export class XunkCalendarModule {
+  /* Gets today's date */
+  public static getToday(): any {
+    return XunkCalendarComponent.getToday();
+  }
 
-    /* Gets today's date */
-    public static getToday(): any {
-      return XunkCalendarComponent.getToday();
-    }
-
-    /** Pad number with zeros */
-    public static zeroPad(num: number, padlen: number, padchar = '0'): string {
-        return XunkCalendarComponent.zeropad(num, padlen, padchar);
-    }
+  /** Pad number with zeros */
+  public static zeroPad(num: number, padlen: number, padchar = "0"): string {
+    return XunkCalendarComponent.zeropad(num, padlen, padchar);
+  }
 }
